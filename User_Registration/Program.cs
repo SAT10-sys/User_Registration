3e2ef2012259438530f0f13bfacddaeef07a59fe
Satantra Tewari <satantra1508@gmail.com>
2020-10-13 00:48:57 +0530

    [satantra] first name is validated

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -7,6 +7,19 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to User Registration Problem ");
+            User_Details userDetails = new User_Details();
+            RegStart1:
+            Console.WriteLine("Enter first name(starting with capitals and minimum three letters) ");
+            string firstName = Console.ReadLine();
+            bool checkFirstName = userDetails.ValidateFirstName(firstName);
+            if(checkFirstName==true)
+                Console.WriteLine(" FIRST NAME: "+firstName);
+            else
+            {
+                Console.WriteLine("Please enter valid first name");
+                goto RegStart1;
+            }
+
         }
     }
 }
---
+++ User_Registration/User_Details.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration
+{
+    public class User_Details
+    {
+        public string REGEX_FIRST_NAME = "^[A-Z]{1,}[A-Za-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRST_NAME);
+        }
+    }
+}
